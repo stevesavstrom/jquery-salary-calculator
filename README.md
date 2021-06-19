d7f60477aabf056c4c4d9112d91e3f0eab7bb95c
Steve Savstrom <steve.savstrom@gmail.com>
2021-06-19 08:52:18 -0500

    js logic completed for collecting inputs, displaying inputs, and cacluating monthly total

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+# Salary Calculator
 
 ## Description
+This application records employee salaries and adds salaries up to report monthly costs.
 
-Your project description goes here. What problem did you solve? How did you solve it?
+The application includes the following features and functionality:
+[x] Input form that collects employee first name, last name, ID number, job title, annual salary.
+
+[x] A 'Submit' button that collects the form information and stores the information to calculate the monthly costs.
+
+[x] Append the information to the DOM and clear the input fields.
+
+[x] Using the stored information, calculate monthly costs and append this to the DOM.
+
+[ ] If total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
+
+[x] The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
+
+[x] Create a delete button.
+
+[ ] Delete button removes an employee from the DOM.
+
+[ ] Create a delete button that removes an employee from the DOM. For Base mode, it does not need to remove that Employee's salary from the reported total.
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
---
+++ client.js
@@ -0,0 +1,104 @@
+// Testing sourced JS file
+console.log( 'js is working');
+// starting with empty employees array
+let employees = [];
+// starting with $0 in car prices
+const totalValue = 0;
+
+$( document ).ready( readyNow );
+
+function readyNow(){
+// handle click event
+$( '#submitButton').on('click', addEmployee);
+
+// target priceOut by id
+let el = $( '#totalOut' )
+el.empty();
+el.append('$', totalValue );
+} // end readyNow
+
+// Adds employee to array when inputs are entered and clicked
+function addEmployee(){
+	let firstName = $("#firstName");
+	let lastName = $("#lastName");
+	let id = $("#id");
+	let title = $("#title");
+	let salary = $("#salary");
+
+	if(
+	firstName.val() == false || 
+	lastName.val() == false || 
+	id.val() == false || 
+	title.val() == false || 
+	salary.val() == false)
+	{alert('Please complete all fields.');} 
+	else {
+		newEmployee(firstName.val(), 
+		lastName.val(), 
+		id.val(), 
+		title.val(), 
+		salary.val());
+	  }
+	// Clears input fields
+	$( "#firstName" ).val('');
+	$( "#lastName" ).val('');
+	$( "#id" ).val('');
+	$( "#title" ).val('');
+	$( "#salary" ).val('');
+	displayEmployees();
+	calculateTotal();
+  }
+
+  function displayEmployees(){
+	console.log( 'in displayEmployees' );
+	// target output by id
+	let el = $('#employeesOut')
+	// empty
+	el.empty();
+	// loop through garage array
+	for (let employee of employees) {
+	  console.log(employee);
+	  // Displays inputs in DOM. Shout out to Matt for help on the image syntax!
+	  el.append(` 
+	  <table>
+		<tr>
+		  <td>${employee.firstName}</td>
+		  <td>${employee.lastName}</td>
+		  <td>${employee.id}</td>
+		  <td>${employee.title}</td>
+		  <td>${employee.salary}</td>
+		  <td><button id="deleteButton">Delete</button></td>
+		</tr>
+	  </table>
+	  `);
+	}
+  } // end displayGarage
+
+  function calculateTotal(){
+	console.log( 'in calculateTotal' );
+	// loop through garage array
+	let totalSalary = 0;
+	for (let i = 0; i < employees.length; i++) {
+	// for each car, add up total of all prices
+	totalSalary += Number( employees[ i ].salary / 12 );
+	} // end for loop
+	console.log( 'totalSalary', totalSalary);
+	// add totalPrices to totalPrice
+	const newTotal = totalValue + totalSalary;
+	// display totalPrice
+	let el = $( '#totalOut' );
+	el.empty();
+	el.append('$', newTotal );
+  } // end calculateTotal
+
+  function newEmployee(firstName, lastName, id, title, salary){
+	const newEmployeeObject = {
+	  firstName: firstName,
+	  lastName: lastName,
+	  id: id,
+	  title: title,
+	  salary: salary
+	}
+	employees.push(newEmployeeObject);
+	console.log('Employees:', employees);
+  } // end newEmployee
